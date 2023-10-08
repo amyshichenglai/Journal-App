869ca62dbc4d3cd61729cb71ecc5e2061eb5e104
seangong <s35gong@uwaterloo.ca>
2023-10-08 15:36:41 -0400

    newnew

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,20 +21,42 @@ import androidx.compose.ui.window.application
 
 
 
+
 // Sample Composable functions for each section
 @Composable
 fun Calendar() {
     Text("Calendar")
 }
 
-@Composable
-fun Summary() {
-    Text("This is the Summary content")
-}
+
 
 @Composable
 fun ToDoList() {
     Text("This is the To-Do-List content")
+}
+
+@Composable
+fun BoxItem(color: Color, text: String) {
+    Box(modifier = Modifier.size(200.dp, 200.dp).background(color)) {
+        Text(text)
+    }
+}
+@Composable
+fun MagicHome() {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(4) { index ->
+            when (index) {
+                0 -> BoxItem(Color.Red, "Summary section")
+                1 -> BoxItem(Color.Yellow, "Calendar section")
+                2 -> BoxItem(Color.Blue, "Todo section")
+                3 -> BoxItem(Color.Green, "Notes section")
+            }
+        }
+    }
 }
 
 @Composable
@@ -42,34 +67,38 @@ fun Notes() {
 @Composable
 fun AppLayout() {
     val (selectedSection, setSelectedSection) = remember { mutableStateOf("Section 1") }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Button(onClick = { setSelectedSection("Calendar") }, modifier = Modifier.weight(1f).padding(16.dp)) {
-            Text("Calendar")
+    Row() {
+        Column(
+//            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = { setSelectedSection("Home") }, modifier = Modifier.weight(1f).padding(16.dp)) {
+                Text("Home")
+            }
+            Button(onClick = { setSelectedSection("Calendar") }, modifier = Modifier.weight(1f).padding(16.dp)) {
+                Text("Calendar")
+            }
+            Button(onClick = { setSelectedSection("Summary") }, modifier = Modifier.weight(1f).padding(16.dp)) {
+                Text("Summary")
+            }
+            Button(onClick = { setSelectedSection("To-Do-List") }, modifier = Modifier.weight(1f).padding(16.dp)) {
+                Text("To-Do-List")
+            }
+            Button(onClick = { setSelectedSection("Notes") }, modifier = Modifier.weight(1f).padding(16.dp)) {
+                Text("Notes")
+            }
         }
-        Button(onClick = { setSelectedSection("Summary") }, modifier = Modifier.weight(1f).padding(16.dp)) {
-            Text("Summary")
-        }
-        Button(onClick = { setSelectedSection("To-Do-List") }, modifier = Modifier.weight(1f).padding(16.dp)) {
-            Text("To-Do-List")
-        }
-        Button(onClick = { setSelectedSection("Notes") }, modifier = Modifier.weight(1f).padding(16.dp)) {
-            Text("Notes")
-        }
-
         Box(
             modifier = Modifier.weight(3f).fillMaxWidth(),
             contentAlignment = Alignment.Center
         ) {
             when (selectedSection) {
+                "Home" -> MagicHome()
                 "Calendar" -> Calendar()
                 "Summary" -> Summary()
                 "To-Do-List" -> ToDoList()
                 "Notes" -> Notes()
-                else -> Text(selectedSection)
+                else -> MagicHome()
             }
         }
     }
@@ -82,3 +111,4 @@ fun main() = application {
         }
     }
 }
+
