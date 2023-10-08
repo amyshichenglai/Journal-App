d29fc15c1203d8c27787a7d19fb5cd37b2a4fef2
seangong <s35gong@uwaterloo.ca>
2023-10-08 13:57:33 -0400

    hello

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,15 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
 
 
 // Sample Composable functions for each section
@@ -42,9 +36,9 @@ fun Notes() {
 @Composable
 fun AppLayout() {
     val (selectedSection, setSelectedSection) = remember { mutableStateOf("Section 1") }
-
-
-        Column(
+    Row(
+        Modifier.fillMaxSize()){
+            Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
@@ -59,22 +53,9 @@ fun AppLayout() {
             }
             Button(onClick = { setSelectedSection("Notes") }, modifier = Modifier.weight(1f).padding(16.dp)) {
                 Text("Notes")
-            }
+            }}
         }
-        Box(
-            modifier = Modifier.weight(3f).fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            when (selectedSection) {
-                "Calendar" -> Calendar()
-                "Summary" -> Summary()
-                "To-Do-List" -> ToDoList()
-                "Notes" -> Notes()
-                else -> Text(selectedSection)
-            }
-        }
-    }
-
+}
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
