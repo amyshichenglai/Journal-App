ff33b25f05529751d8f4abcceeadafb88b2d56f3
Amy L <amyl@Amys-MacBook-Air.local>
2023-10-08 17:09:24 -0400

    I love JAva

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
 plugins {
-    kotlin("jvm") version "1.9.10" // Update to Kotlin Compiler 1.9.10
-    id("org.jetbrains.compose") version "1.5.1" // Update to Compose version 1.5.1
+    kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.compose") version "1.5.3"
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -14,25 +10,14 @@ repositories {
     google()
 }
 
-val ktor_version: String by project
 
 dependencies {
     implementation(compose.desktop.currentOs)
-    implementation("io.vavr:vavr:0.10.4")
-    implementation("io.ktor:ktor-client-core:2.3.5") // Update to Ktor version 2.3.5
-    implementation("io.ktor:ktor-client-cio:2.3.5") // Update to Ktor version 2.3.5
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10") // Update to Kotlin 1.9.10 and JDK 8 variant
-
 }
+
 
 compose.desktop {
     application {
         mainClass = "MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "application"
-            packageVersion = "1.0.0"
-        }
     }
 }
