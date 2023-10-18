785515fa8c4e3c2975efd8cf1f535da074da293f
JUDECHEN1219 <102499176+JUDECHEN1219@users.noreply.github.com>
2023-10-18 17:06:07 -0400

    adding list card

--- src/main/kotlin/note/main_note.kt
+++ src/main/kotlin/note/main_note.kt
@@ -1,5 +1,7 @@
 package note
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
@@ -15,10 +17,24 @@ import com.mohamedrejeb.richeditor.ui.material3.RichTextEditor
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 
+@Composable
+fun myButton(
+    isSelected: Boolean,
+    pickCoor: Color,
+
+
+) {}
 
 @Composable
 fun DocumentToolbar(
@@ -41,7 +57,6 @@ fun DocumentToolbar(
         TextButton(onClick = onItalic, modifier = Modifier.focusable(false)) {
             Text("Italic")
         }
-         // Adds some space between buttons
         TextButton(onClick = onUndo, modifier = Modifier.focusable(false)) {
             Text("Undo")
         }
@@ -90,20 +105,48 @@ fun EditorInterface() {
 
 @Composable
 fun NoteList() {
-    Column() {
-        for (i in 0 until 5) {
-            ListItem(
-                tonalElevation = 34.dp,
-                shadowElevation = 10.dp,
-                headlineContent = { Text("Note${i}") },
-                leadingContent = {
+    val selectedNoteIndex = remember { mutableStateOf<Int?>(null) }
+
+    LazyColumn() {
+        items(20) {
+            val backgroundColor = if (it == selectedNoteIndex.value) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onPrimary
+            val fontWeight = if (it == selectedNoteIndex.value) FontWeight.Bold else FontWeight.Normal
+
+            ElevatedCard (
+                // shadow
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 3.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = backgroundColor
+                ),
+                shape = RoundedCornerShape(8.dp),
+
+
+            ) {
+                Row (modifier = Modifier
+                    .clickable { selectedNoteIndex.value = it }
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                ) {
                     Icon(
                         Icons.Filled.Edit,
                         contentDescription = "Localized description",
+                        modifier = Modifier
+                            .clickable {  }
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 10.dp)
+                    )
+                    Text(
+                        text = "Note ${it+1}",
+                        fontWeight = fontWeight,
+                        modifier = Modifier
+                            .padding(16.dp),
                     )
                 }
-            )
-            Divider()
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+
         }
     }
 }
@@ -116,7 +159,7 @@ fun NotesEditor() {
         Box(
             modifier = Modifier
                 .fillMaxHeight()
-                .weight(0.15f)  // 10% of parent's height
+                .weight(0.15f)  // 15% of parent's height
                 .padding(top = 70.dp)
             // other modifiers, content, etc.
         ) {
@@ -126,7 +169,7 @@ fun NotesEditor() {
         Box(
             modifier = Modifier
                 .fillMaxHeight()
-                .weight(0.85f)  // 90% of parent's height
+                .weight(0.85f)  // 85% of parent's height
             // other modifiers, content, etc.
         ) {
             EditorInterface()
